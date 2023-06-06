ae6c01a2f33bca257c88dd565a39d1602fd1f728
leedongyeop <ldy_1204@naver.com>
2023-06-06 17:46:02 +0900

    fix : 이메일 유효성 검사 추가
    웹 클라이언트로부터 받는 SignupRequest에서 받는 email 유효성 검사를 위한 애너테이션을 추가합니다.
    `@Email` 과 함께 `@NotBlank`를 사용한 이유는 해당 어노테이션은 null과 ""(공백)을 유효하다고 판단하기 때문에
    이를 방지하기 위해 `@NotBlank`를 함께 사용하였습니다.
    
    참고하면 좋은 자료입니다.
    - https://bellog.tistory.com/134

--- src/main/java/io/wisoft/avocadobackendhexagonal/domain/auth/adapter/in/web/dto/SignupMemberRequest.java
+++ src/main/java/io/wisoft/avocadobackendhexagonal/domain/auth/adapter/in/web/dto/SignupMemberRequest.java
@@ -1,9 +1,11 @@
 package io.wisoft.avocadobackendhexagonal.domain.auth.adapter.in.web.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record SignupMemberRequest(
-        @NotBlank String email,
+        @NotBlank @Email
+        String email,
         @NotBlank String nickname,
         @NotBlank String password,
         @NotBlank String phoneNumber,
--- src/main/java/io/wisoft/avocadobackendhexagonal/domain/auth/adapter/in/web/dto/SignupStaffRequest.java
+++ src/main/java/io/wisoft/avocadobackendhexagonal/domain/auth/adapter/in/web/dto/SignupStaffRequest.java
@@ -1,10 +1,12 @@
 package io.wisoft.avocadobackendhexagonal.domain.auth.adapter.in.web.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SignupStaffRequest(
-        @NotBlank String name,
+        @NotBlank @Email
+        String name,
         @NotBlank String email,
         @NotBlank String password,
         @NotBlank String license_path,
