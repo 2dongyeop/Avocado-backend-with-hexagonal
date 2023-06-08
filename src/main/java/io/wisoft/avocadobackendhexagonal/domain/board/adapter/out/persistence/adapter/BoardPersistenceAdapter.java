c83f7ecd016ae2ab2287c555ec903bf7bbc8da79
leedongyeop <ldy_1204@naver.com>
2023-06-08 14:54:39 +0900

    feat : BoardMapper를 의존성 주입이 아닌 static 메서드 호출 방식으로 변환

--- src/main/java/io/wisoft/avocadobackendhexagonal/domain/board/adapter/out/persistence/adapter/BoardPersistenceAdapter.java
+++ src/main/java/io/wisoft/avocadobackendhexagonal/domain/board/adapter/out/persistence/adapter/BoardPersistenceAdapter.java
@@ -21,28 +21,27 @@ import java.util.stream.Collectors;
 public class BoardPersistenceAdapter implements SaveBoardPort, LoadBoardPort, DeleteBoardPort {
 
     private final BoardRepository boardRepository;
-    private final BoardMapper boardMapper;
 
     @Override
     public Long save(final Board board) {
-        return boardRepository.save(boardMapper.boardToBoardEntity(board)).getId();
+        return boardRepository.save(BoardMapper.boardToBoardEntity(board)).getId();
     }
 
     @Override
     public Board findById(final Long boardId) {
         final BoardEntity boardEntity = boardRepository.findById(boardId).orElseThrow(NotFoundBoardException::new);
-        return boardMapper.boardEntityToBoard(boardEntity);
+        return BoardMapper.boardEntityToBoard(boardEntity);
     }
 
     @Override
     public Page<Board> findAll(final Pageable pageable) {
         return new PageImpl<>(boardRepository.findAll(pageable).stream()
-                .map(boardEntity -> BoardMapper.boardEntityToBoard(boardEntity))
+                .map(BoardMapper::boardEntityToBoard)
                 .collect(Collectors.toList()));
     }
 
     @Override
     public void delete(final Board board) {
-        boardRepository.delete(boardMapper.boardToBoardEntity(board));
+        boardRepository.delete(BoardMapper.boardToBoardEntity(board));
     }
 }
