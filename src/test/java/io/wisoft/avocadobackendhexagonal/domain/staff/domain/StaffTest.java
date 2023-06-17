9c4549129d1c65c9378973efff45d95ff6e5259d
leedongyeop <ldy_1204@naver.com>
2023-06-17 20:58:40 +0900

    fix : 비밀번호 변경시 누락된 암호화 과정 추가

---
+++ src/test/java/io/wisoft/avocadobackendhexagonal/domain/staff/domain/StaffTest.java
@@ -0,0 +1,22 @@
+package io.wisoft.avocadobackendhexagonal.domain.staff.domain;
+
+import org.junit.jupiter.api.Test;
+
+class StaffTest {
+
+    @Test
+    public void updateHospital() throws Exception {
+        //given -- 조건
+        final Staff staff = Staff.createStaff(
+                1L,
+                "name",
+                "email",
+
+                )
+
+        //when -- 동작
+
+        //then -- 검증
+    }
+
+}
